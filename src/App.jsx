4beb8b57f17110c027b0c9e6be062369b595e174
Rakeshkumar <rakesh11314311@gmail.com>
2025-06-17 08:11:56 +0530

    Modified Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Create from './Create'
 import Update from './Update'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/create' element={<Create />}></Route>
           <Route path='/edit/:id' element={<Update />}></Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   )
 }
